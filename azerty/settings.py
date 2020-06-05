60c858226207b66e6a7a47907752a74c3b434192
lucaslndr <lucas.landru@isen.yncrea.fr>
2020-06-05 17:43:41 +0200

    cors

--- azerty/settings.py
+++ azerty/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
 
     'rest_framework',
-    'django_filters',
+    #'django_filters',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'azerty.urls'
