2c75476cc511d46f12e00bee7fe87a1d5f03058a
lucaslndr <lucas.landru@isen.yncrea.fr>
2020-06-08 16:58:57 +0200

    pagination
    page to 100

--- azerty/settings.py
+++ azerty/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 100
 }
 
 # CORS_ORIGIN_WHITELIST = ['*']
