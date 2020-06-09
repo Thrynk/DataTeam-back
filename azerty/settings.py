c270f1340a9cba0564fcbafddcb8d70ac15d0fc9
lucaslndr <lucas.landru@isen.yncrea.fr>
2020-06-09 14:45:15 +0200

    page

--- azerty/settings.py
+++ azerty/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 200
 }
 
 # CORS_ORIGIN_WHITELIST = ['*']
