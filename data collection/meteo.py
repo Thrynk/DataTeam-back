21679b0038fb74f86ddc8b3aa7481c91446bd1e3
Thrynk <thocarin@gmail.com>
2020-06-12 10:04:42 +0200

    removed data collection to put it on another branch

--- data collection/meteo.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import requests
-from requests.auth import HTTPBasicAuth
-r = requests.get('https://api.meteomatics.com/2020-06-11T15:30:00ZP7D:PT2H/t_0m:C/50.6,3.06/json?model=mix', auth=HTTPBasicAuth('isen_meunier', 'rT2ql81CVUzpS'))
-
-r.json() 
